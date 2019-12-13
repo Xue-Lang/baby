311d0b54e5f048032ce3572632e2676a249c32a3
Xue-Lang <2657127062@qq.com>
2019-12-13 16:00:18 +0800

    11

--- src/RegisterPage/registerPage.js
+++ src/RegisterPage/registerPage.js
@@ -1,10 +1,51 @@
 import React from 'react';
-import { Button, Input, Row, Col } from 'antd';
+import { Button, Input, Row, Col,message } from 'antd';
 import { Link } from 'react-router-dom'
 var registerPage = require("./registerPage.css")
 
 
 export default class RegisterPage extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={}
+    }
+    changeValue=(e)=>{
+        this.setState({
+            [e.target.name]:e.target.value
+        })
+    }
+    upload = ()=>{
+        //XHR
+        var xhr = new XMLHttpRequest()
+        var date = {
+            "username":this.usernaem,
+            "password":this.password,
+        }
+        //open连接
+        xhr.open("post","/user/register") 
+        //配置响应函数
+        xhr.onreadystatechange=function(){
+            if(xhr.readyState==4){
+                if(xhr.status==200){
+                    console.log(xhr.responseText)
+                    var result = JSON.parse(xhr.responseText)
+                    if(result.state==2){
+                        message.info(xhr.responseText)
+                    }else{
+                        message.info(xhr.responseText)
+                       // this.props.history.push("/homepage")
+                    }
+                }else{
+                    message.info(xhr.status)
+                }
+            }
+        }
+        //发送数据
+        xhr.setRequestHeader('content-type','application/json');
+
+        xhr.send(JSON.stringify(date))
+        //fetch
+    }
     render() {
         return (
             <div className={registerPage.all}>
@@ -19,14 +60,14 @@ export default class RegisterPage extends React.Component {
                     </div>
                         <div className={registerPage.text3}>
                             用户名：
-                        <Input size="small" className={registerPage.number}></Input>
+                        <Input size="small" className={registerPage.number} type="text"  name="username" value={this.state.username} onChange={e=>this.changeValue(e)}></Input>
                         </div>
                         <div className={registerPage.text4}>
                             设置密码：
-                        <Input.Password placeholder="请输入密码" size="small" className={registerPage.Password} />
+                        <Input.Password placeholder="请输入密码" size="small" className={registerPage.Password}  type="password" name="password" value={this.state.Password} onChange={e=>this.changeValue(e)}/>
                         </div>
                         <div className={registerPage.registe}>
-                            <Button type="primary" shape="round" ><Link to="initial">注册</Link></Button>
+                            <Button type="primary" shape="round" onClick={this.upload}>注册</Button>
                         </div>
                         <div className={registerPage.text6}>
                             已有账号？
